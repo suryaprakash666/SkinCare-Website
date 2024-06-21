f673e637241f081500d317c3e3ff8309bc11e61b
Danisha2002sahu <danishasahu86@gmail.com>
2024-06-21 15:33:55 +0530

    Homepage Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {LoginPage,StarterQuery} from './LoginPage';
+import Navbar from './homepage';
 
 function App() {
   return (
     <div className="App">
       <StarterQuery/>
       <LoginPage />
+      <Navbar />
+
     </div>
   );
 }
